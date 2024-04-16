f83e07511d7e070b61cbce7f4f2afa0256834160
jd <jonathand@keyweo.com>
2024-04-16 23:49:01 +0200

    MAJ

--- SCENES/titleScreen.js
+++ SCENES/titleScreen.js
@@ -14,6 +14,41 @@ class TitleScreen {
     static init() {
         canvas.style.backgroundColor = CANVAS_ORIGIN_COLOR;
 
+        let originX = 320;
+        let offX = 0;
+        let y = 10;
+        let offY = 3; 
+        for (let i = 0; i < 9; i++) {
+            if (isPair(i)) {
+                // y = 10;
+            } else {
+                // y = 12
+            }
+            let letter = new Sprite({ w: 26, h: 36 }, 1+offX, y+offY, null, "TitleScreen");
+            letter.addAnimation("normal", { x: originX, y: 384 });
+            letter.changeAnimation("normal");
+            
+
+            if (isPair(i)) {
+                letter.setOriginPos({ x: 1+offX, y: y+offY });
+                letter.setOriginDestination({ x: 1+offX, y: y-3 });
+                letter.setMovingType(Sprite.MOVING_TYPE.ComeAndGo);
+                letter.setDestination({ x:  1+offX, y: y-3 });  
+            } else {
+                letter.setOriginPos({ x: 1+offX, y: y+offY });
+                letter.setOriginDestination({ x: 1+offX, y: y+3 });
+                letter.setMovingType(Sprite.MOVING_TYPE.ComeAndGo);
+                letter.setDestination({ x:  1+offX, y: y+3 });
+            }
+            offY = -offY;
+
+            TitleScreen.list.push(letter);
+            originX += 26;
+            offX += 20;
+
+
+        }
+
         let normalBtn = new Button({ w: 60, h: 24, v: 7}, centerX(60), 100, null, { cb: TitleScreen.start, arg: 1 }, "TitleScreen", TitleScreen.STATE.Main, "NORMAL", 1);
         normalBtn.setFreeLabel();
         normalBtn.setFontColor(CARD_BTN_SDW_COLOR, BLACK_COLOR, CARD_BTN_SDW_COLOR);
