0ed78eb3b072bb4785f4f8a9d477acd4f4056632
jd <jonathand@keyweo.com>
2024-04-17 08:41:47 +0200

    MAJ

--- SCENES/titleScreen.js
+++ SCENES/titleScreen.js
@@ -19,16 +19,10 @@ class TitleScreen {
         let y = 10;
         let offY = 3; 
         for (let i = 0; i < 9; i++) {
-            if (isPair(i)) {
-                // y = 10;
-            } else {
-                // y = 12
-            }
             let letter = new Sprite({ w: 26, h: 36 }, 1+offX, y+offY, null, "TitleScreen");
             letter.addAnimation("normal", { x: originX, y: 384 });
             letter.changeAnimation("normal");
             
-
             if (isPair(i)) {
                 letter.setOriginPos({ x: 1+offX, y: y+offY });
                 letter.setOriginDestination({ x: 1+offX, y: y-3 });
@@ -45,29 +39,79 @@ class TitleScreen {
             TitleScreen.list.push(letter);
             originX += 26;
             offX += 20;
-
-
         }
+
+        let by = new Sprite({ w: 9, h: 10 }, 132, 67, null, "TitleScreen");
+        by.addAnimation("normal", { x: 0, y: 52 });
+        by.changeAnimation("normal");
+        TitleScreen.list.push(by);
+
+        let jadona = new Sprite({ w: 38, h: 35 }, 148, 54, null, "TitleScreen");
+        jadona.addAnimation("normal", { x: 0, y: 0 });
+        jadona.changeAnimation("normal");
+        TitleScreen.list.push(jadona);
 
         let normalBtn = new Button({ w: 60, h: 24, v: 7}, centerX(60), 100, null, { cb: TitleScreen.start, arg: 1 }, "TitleScreen", TitleScreen.STATE.Main, "NORMAL", 1);
         normalBtn.setFreeLabel();
         normalBtn.setFontColor(CARD_BTN_SDW_COLOR, BLACK_COLOR, CARD_BTN_SDW_COLOR);
         TitleScreen.list.push(normalBtn.getSprite());
+        
+        let firstFrameSpeed = 1;
+
+        let normalBtnLeft = new Sprite({ w: 16, h: 16 }, -20, 3, normalBtn, "TitleScreen");
+        normalBtnLeft.addAnimation("normal", { x: 0, y: 544 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        normalBtnLeft.changeAnimation("normal");
+        TitleScreen.list.push(normalBtnLeft);
+        let normalBtnRight = new Sprite({ w: 16, h: 16 }, normalBtn.width + 3, 3, normalBtn, "TitleScreen");
+        normalBtnRight.addAnimation("normal", { x: 0, y: 544 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        normalBtnRight.changeAnimation("normal");
+        TitleScreen.list.push(normalBtnRight);
+
 
         let pixelBtn = new Button({ w: 60, h: 24, v: 7}, centerX(60), 130, null, { cb: TitleScreen.start, arg: 2 }, "TitleScreen", TitleScreen.STATE.Main, "PIXEL", 1);
         pixelBtn.setFreeLabel();
         pixelBtn.setFontColor(CARD_BTN_SDW_COLOR, BLACK_COLOR, CARD_BTN_SDW_COLOR);
         TitleScreen.list.push(pixelBtn.getSprite());
 
+        let pixelBtnLeft = new Sprite({ w: 16, h: 16 }, -20, 3, pixelBtn, "TitleScreen");
+        pixelBtnLeft.addAnimation("normal", { x: 0, y: 560 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        pixelBtnLeft.changeAnimation("normal");
+        TitleScreen.list.push(pixelBtnLeft);
+        let pixelBtnRight = new Sprite({ w: 16, h: 16 }, pixelBtn.width + 3, 3, pixelBtn, "TitleScreen");
+        pixelBtnRight.addAnimation("normal", { x: 0, y: 560 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        pixelBtnRight.changeAnimation("normal");
+        TitleScreen.list.push(pixelBtnRight);
+
+
         let kanjiBtn = new Button({ w: 60, h: 24, v: 7}, centerX(60), 160, null, { cb: TitleScreen.start, arg: 3 }, "TitleScreen", TitleScreen.STATE.Main, "漢字", 1);
         kanjiBtn.setFreeLabel();
         kanjiBtn.setFontColor(CARD_BTN_SDW_COLOR, BLACK_COLOR, CARD_BTN_SDW_COLOR);
         TitleScreen.list.push(kanjiBtn.getSprite());
 
+        let kanjiBtnLeft = new Sprite({ w: 16, h: 16 }, -20, 3, kanjiBtn, "TitleScreen");
+        kanjiBtnLeft.addAnimation("normal", { x: 0, y: 576 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        kanjiBtnLeft.changeAnimation("normal");
+        TitleScreen.list.push(kanjiBtnLeft);
+        let kanjiBtnRight = new Sprite({ w: 16, h: 16 }, kanjiBtn.width + 3, 3, kanjiBtn, "TitleScreen");
+        kanjiBtnRight.addAnimation("normal", { x: 0, y: 576 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        kanjiBtnRight.changeAnimation("normal");
+        TitleScreen.list.push(kanjiBtnRight);
+
+
         let hanziBtn = new Button({ w: 60, h: 24, v: 7}, centerX(60), 190, null, { cb: TitleScreen.start, arg: 4 }, "TitleScreen", TitleScreen.STATE.Main, "中文", 1);
         hanziBtn.setFreeLabel();
         hanziBtn.setFontColor(CARD_BTN_SDW_COLOR, BLACK_COLOR, CARD_BTN_SDW_COLOR);
         TitleScreen.list.push(hanziBtn.getSprite());
+
+        let hanziBtnLeft = new Sprite({ w: 16, h: 16 }, -20, 3, hanziBtn, "TitleScreen");
+        hanziBtnLeft.addAnimation("normal", { x: 0, y: 592 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        hanziBtnLeft.changeAnimation("normal");
+        TitleScreen.list.push(hanziBtnLeft);
+        let hanziBtnRight = new Sprite({ w: 16, h: 16 }, hanziBtn.width + 3, 3, hanziBtn, "TitleScreen");
+        hanziBtnRight.addAnimation("normal", { x: 0, y: 592 }, 4, [firstFrameSpeed, 0.05, 0.1, 0.05]);
+        hanziBtnRight.changeAnimation("normal");
+        TitleScreen.list.push(hanziBtnRight);
+
 
         Button.resetTypeState("TitleScreen", TitleScreen.STATE.Main);
     }
